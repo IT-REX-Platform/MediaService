12ef24d796c16d1e25ef087f42be487ad99f3de1
Noeliel <noelieldev@gmail.com>
2021-01-26 19:44:51 +0100

    Watch video fundamentals

--- src/main/java/de/uni_stuttgart/it_rex/media/written/video/VideoController.java
+++ src/main/java/de/uni_stuttgart/it_rex/media/written/video/VideoController.java
@@ -1,7 +1,16 @@
 package de.uni_stuttgart.it_rex.media.written.video;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRange;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +25,12 @@ public class VideoController {
      * Service for storing videos.
      */
     private final VideoStorageService videoStorageService;
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER =
+        LoggerFactory.getLogger(VideoController.class);
 
     /**
      * Constructor.
@@ -45,5 +60,37 @@ public class VideoController {
             "You successfully uploaded " + file.getOriginalFilename() + "!");
 
         return "redirect:/";
+    }
+
+    @GetMapping("/videos/download/{filename:.+}")
+    public ResponseEntity<Resource> downloadVideo(@PathVariable final String filename, @RequestHeader
+        HttpHeaders headers) {
+
+        LOGGER.info("Trying to download file " + filename);
+
+        long chosenFileLength = videoStorageService.getLength(filename);
+
+        HttpRange range;
+
+        try {
+            range = headers.getRange().get(0);
+        } catch (IndexOutOfBoundsException e) {
+            range = HttpRange.parseRanges("bytes=0-").get(0);
+        }
+
+        long start = range.getRangeStart(chosenFileLength);
+        long end = range.getRangeEnd(chosenFileLength);
+        long length = end - start + 1;
+
+        LOGGER.info("Start: " + start + " End: " + end + " Length: " + length);
+
+        Resource file = videoStorageService.loadAsResource(filename, start, length);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add(HttpHeaders.CONTENT_DISPOSITION,
+            "inline; filename=\"" + filename + "\"");
+        responseHeaders.add("Content-Type", "video/mp4");
+
+        return ResponseEntity.ok().headers(responseHeaders).body(file);
     }
 }
--- src/main/java/de/uni_stuttgart/it_rex/media/written/video/VideoStorageService.java
+++ src/main/java/de/uni_stuttgart/it_rex/media/written/video/VideoStorageService.java
@@ -1,20 +1,27 @@
 package de.uni_stuttgart.it_rex.media.written.video;
 
 import de.uni_stuttgart.it_rex.media.written.StorageException;
+import de.uni_stuttgart.it_rex.media.written.StorageFileNotFoundException;
 import io.minio.BucketExistsArgs;
+import io.minio.GetObjectArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.StatObjectArgs;
+import io.minio.StatObjectResponse;
 import io.minio.errors.MinioException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -230,5 +237,68 @@ public class VideoStorageService {
     public final void setRootLocation(
         @Value("${minio.root-location}") final Path newLocation) {
         this.rootLocation = newLocation;
+    }
+
+    /**
+     * Get the size of a stored file
+     * @param filename Name of the file to stat
+     * @return
+     */
+    public long getLength(String filename) {
+
+        try {
+            StatObjectResponse stat = minioClient.statObject(
+                StatObjectArgs.builder()
+                    .bucket(rootLocation.toString())
+                    .object(filename)
+                    .build());
+
+            return stat.size();
+
+        } catch (InvalidKeyException e) {
+        } catch (NoSuchAlgorithmException e) {
+        } catch (MinioException e) {
+            throw new StorageException("Failed to read stored files", e);
+        } catch (IOException e) {
+        }
+
+        return 0l;
+    }
+
+    /**
+     * Get the contents of a stored file
+     * @param filename Name of file to load
+     * @param offset Starting offset to read from
+     * @param length How many bytes to read
+     * @return *length* bytes of requested file starting from *offset* if file exists, null otherwise
+     */
+    public Resource loadAsResource(String filename, long offset, long length) {
+
+        // get object given the bucket and object name
+        try {
+            InputStream stream = minioClient.getObject(
+                GetObjectArgs.builder()
+                    .bucket(rootLocation.toString())
+                    .object(filename)
+                    .offset(offset)
+                    .length(length)
+                    .build());
+
+            // Read data from stream
+            InputStreamResource
+                resource = new InputStreamResource(stream, filename);
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new StorageFileNotFoundException("Could not read file: " + filename);
+            }
+        } catch (InvalidKeyException e) {
+        } catch (NoSuchAlgorithmException e) {
+        } catch (MinioException e) {
+            throw new StorageException("Failed to read stored files", e);
+        } catch (IOException e) {
+        }
+
+        return null;
     }
 }
