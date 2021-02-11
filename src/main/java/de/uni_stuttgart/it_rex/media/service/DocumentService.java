75afcf07435af6067490d87eec361fb2239f5850
GarondEisenfaust <chr.schr@web.de>
2021-02-11 16:11:20 +0100

    Generated Document entity

---
+++ src/main/java/de/uni_stuttgart/it_rex/media/service/DocumentService.java
@@ -0,0 +1,85 @@
+package de.uni_stuttgart.it_rex.media.service;
+
+import de.uni_stuttgart.it_rex.media.domain.Document;
+import de.uni_stuttgart.it_rex.media.repository.DocumentRepository;
+import de.uni_stuttgart.it_rex.media.service.dto.DocumentDTO;
+import de.uni_stuttgart.it_rex.media.service.mapper.DocumentMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Service Implementation for managing {@link Document}.
+ */
+@Service
+@Transactional
+public class DocumentService {
+
+    private final Logger log = LoggerFactory.getLogger(DocumentService.class);
+
+    private final DocumentRepository documentRepository;
+
+    private final DocumentMapper documentMapper;
+
+    public DocumentService(DocumentRepository documentRepository, DocumentMapper documentMapper) {
+        this.documentRepository = documentRepository;
+        this.documentMapper = documentMapper;
+    }
+
+    /**
+     * Save a document.
+     *
+     * @param documentDTO the entity to save.
+     * @return the persisted entity.
+     */
+    public DocumentDTO save(DocumentDTO documentDTO) {
+        log.debug("Request to save Document : {}", documentDTO);
+        Document document = documentMapper.toEntity(documentDTO);
+        document = documentRepository.save(document);
+        return documentMapper.toDto(document);
+    }
+
+    /**
+     * Get all the documents.
+     *
+     * @return the list of entities.
+     */
+    @Transactional(readOnly = true)
+    public List<DocumentDTO> findAll() {
+        log.debug("Request to get all Documents");
+        return documentRepository.findAll().stream()
+            .map(documentMapper::toDto)
+            .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+
+    /**
+     * Get one document by id.
+     *
+     * @param id the id of the entity.
+     * @return the entity.
+     */
+    @Transactional(readOnly = true)
+    public Optional<DocumentDTO> findOne(Long id) {
+        log.debug("Request to get Document : {}", id);
+        return documentRepository.findById(id)
+            .map(documentMapper::toDto);
+    }
+
+    /**
+     * Delete the document by id.
+     *
+     * @param id the id of the entity.
+     */
+    public void delete(Long id) {
+        log.debug("Request to delete Document : {}", id);
+        documentRepository.deleteById(id);
+    }
+}
